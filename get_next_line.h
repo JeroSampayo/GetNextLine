ac447377acaa1e6b943fb9bec9faeb7ce3a20fe4
JeronimoMiras <113709509+JeroSampayo@users.noreply.github.com>
2023-05-02 21:32:00 +0200

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 12:52:05 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/05/02 21:28:16 by jmiras-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int i);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
