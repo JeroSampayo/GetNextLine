73db9bfe2bfe4816b7a1e6e5616437c407a56dbf
JeronimoMiras <113709509+JeroSampayo@users.noreply.github.com>
2023-05-16 18:34:55 +0200

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 12:52:05 by jmiras-s          #+#    #+#             */
+/*   Updated: 2023/05/16 18:01:48 by jmiras-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+
+char	*get_next_line(int fd);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int i);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+void	*ft_free(char **str);
+
+#endif
