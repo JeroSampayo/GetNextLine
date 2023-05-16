ae587d94bc008479903bceefad088009861cb768
JeronimoMiras <113709509+JeroSampayo@users.noreply.github.com>
2023-05-16 18:05:31 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jmiras-s <jmiras-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 12:52:05 by jmiras-s          #+#    #+#             */
-/*   Updated: 2023/05/13 18:08:03 by jmiras-s         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:25:23 by jmiras-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
+# include <string.h>
 
 char	*get_next_line(int fd);
 char	*ft_substr(char *s, unsigned int start, size_t len);
 int		ft_strlen(char *str);
 char	*ft_strchr(char *s, int i);
-char	*ft_strdup(char *s);
 char	*ft_strjoin(char *s1, char *s2);
 void	*ft_free(char **str);
 
